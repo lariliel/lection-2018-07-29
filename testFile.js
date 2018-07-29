cc909a85fca6b3118b48bd3a1fef3b868082f3db
Lari <haoogame@gmail.com>
2018-07-29 10:34:44 +0300

    Пример для JS

---
+++ testFile.js
@@ -0,0 +1,9 @@
+/**
+ * Represents a book.
+ * @constructor
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ * @see http://usejsdoc.org/about-getting-started.html
+ */
+function Book(title, author) {
+}
